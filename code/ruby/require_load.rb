84b0476a434317857be1343b7751638fda85d640
Den Arustamov <darus@softserveinc.com>
2022-01-17 22:25:34 +0200

    upd

---
+++ code/ruby/require_load.rb
@@ -0,0 +1,11 @@
+# will be loaded just at once
+# require '/home/darus/WORK/helpex/code/ruby/require_load_1.rb'
+# require "#{__dir__}/require_load_1"
+# can use this for current application path
+require_relative 'require_load_1'
+
+# will be loaded every time
+# load "#{__dir__}/require_load_1.rb"
+# load "#{__dir__}/require_load_1.rb"
+
+Bbb.lol
